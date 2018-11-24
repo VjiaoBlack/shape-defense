c2d1fda8a7bb7288eba5bd94c1c403fe3f165d4b
Victor Jiao <VjiaoBlack@gmail.com>
2018-11-23 20:02:38 -0800

    ecs, refactor: initialize plan for ecs refactor, start of #4
    I tried to just do the refactor in one go, but it was a bit tough.
    I think it'll be a good idea to make a separate branch where I can
    slowly implement the new structure alongside the old, and then
    slowly add more functionality to the new structure until I can
    completely switch over.
    
    Being able to make many small changes instead of just one big change
    will also help me get a better sense of the complexity of the new
    code, in terms of anything I'll have to change in the existing
    allocation structures, etc. Some kind of memory allocator might be
    required.

--- src/TdECS/TdECSSystems/PlanningSystem.hpp
+++ src/TdECS/TdECSSystems/PlanningSystem.hpp
@@ -18,9 +18,12 @@
 class PlanningSystem {
  public:
   std::vector<std::unique_ptr<Pathing>> m_pathingComponents;
-
   std::vector<std::unique_ptr<Attack>> m_attackComponents;
   std::vector<std::unique_ptr<LaserShooter>> m_laserComponents;
+
+  std::vector<Pathing> m_pathingComponentsTEMP;
+  std::vector<Attack> m_attackComponentsTEMP;
+  std::vector<LaserShooter> m_laserComponentsTEMP;
 
   void update(Game *game, System* system);
 };
