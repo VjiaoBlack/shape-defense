250a73d2689bee268a836394ced2180c362cc994
Victor Jiao <VjiaoBlack@gmail.com>
2018-12-25 17:57:33 -0800

    ecs: add type traits for entity color and size

--- src/TdECS/EntityType.hpp
+++ src/TdECS/EntityType.hpp
@@ -1,11 +1,13 @@
 #pragma once
-/** 
+/**
  * EntityType.hpp
  *  
  * Victor Jiao
  *
  * <DETAILS>
  */
+
+#include <Graphics.hpp>
 
 enum class EntityType {
   //// Default
@@ -21,3 +23,40 @@ enum class EntityType {
   ENEMY
 };
 
+template<class T>
+struct EntityShape {
+  SDL_Color c = 0x00000000;
+  int w = 0;
+  int h = 0;
+};
+
+template<EntityType::BASE>
+struct EntityShape {
+  SDL_Color c = 0xFFFFFFFF;
+  int w = 48;
+  int h = 48;
+};
+
+
+template<EntityType::TOWER>
+struct EntityShape {
+  SDL_Color c = 0xFFFFFFFF;
+  int w = 32;
+  int h = 32;
+};
+
+
+template<EntityType::WALL>
+struct EntityShape {
+  SDL_Color c = 0xFFFFFFFF;
+  int w = 16;
+  int h = 16;
+};
+
+
+template<EntityType::ENEMY>
+struct EntityShape {
+  SDL_Color c = 0xFFC06060;
+  int w = 16;
+  int h = 16;
+};
