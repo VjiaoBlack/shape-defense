70f1e5449004cfdb593c72b7f1074e49fc5a9893
Victor Jiao <VjiaoBlack@gmail.com>
2018-12-26 23:06:14 -0800

    bug: fix segfault due to no attack component when wall dies
    need to consider better way of
    1) getting errors when components are badly used (maybe debug mode
       vs performance mode?)
    2) using the backtrace functionality better

--- src/TdECS/TdECSSystems/HealthSystem.cpp
+++ src/TdECS/TdECSSystems/HealthSystem.cpp
@@ -26,13 +26,13 @@ void HealthSystem::update(Game *game, System* system) {
     }
     if (c.m_curHealth <= 0.0) {
       // if enemy, add to player's money
-      if (system->getEnt(c.m_entID)->get<Attack>()->m_team == Attack::ENEMY) {
+      if (system->getEnt(c.m_entID)->has<Attack>() &&
+          system->getEnt(c.m_entID)->get<Attack>()->m_team == Attack::ENEMY) {
         game->m_curMoney += 5.0;
         if (game->m_curMoney > game->m_maxMoney) {
           game->m_curMoney = game->m_maxMoney;
         }
       }
-
       system->m_collisions.m_qtree->removeEntID(game, system, c.m_entID);
       system->getEnt(c.m_entID)->die();
       int id = c.m_entID;
--- src/defense.cpp
+++ src/defense.cpp
@@ -10,8 +10,29 @@
 #define GLM_INLINE
 
 #include "Game.hpp"
+#include <execinfo.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+void handler(int sig) {
+  constexpr int k_max_entries = 32;
+  void *array[k_max_entries];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, k_max_entries);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 
 int main(int argv, char *argc[]) {
+  signal(SIGSEGV, handler);   // install our handler
+
   shared_ptr<Game> renderer = make_shared<Game>();
   renderer->run();
   renderer.reset();
